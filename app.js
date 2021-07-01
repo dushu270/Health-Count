bdf16366303d735255eca1fb3f09e86373bc4e49
dushu270 <dushu270@gmail.com>
2021-07-01 16:07:55 +0530

    procfile and package.json and port modified

--- app.js
+++ app.js
@@ -32,7 +32,14 @@ app.use((req, res, next) => {
     res.status(404).render("error")
    })
 
+   let port = process.env.PORT;
+   if (port == null || port == "") {
+     port = 3000;
+   }
+
    
-app.listen(3000,function(req,res){
+   
+
+app.listen(port,function(req,res){
     console.log("server started")
 })    
